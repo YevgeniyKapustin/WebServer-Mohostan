3da69854aa44172d1604b3763d549de5a8266c0d
Kapusta-fairy <dknenorm63@gmail.com>
2023-04-27 11:43:57 +0300

    add post and get query

--- src/commands/controller.py
+++ src/commands/controller.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from fastapi_cache.decorator import cache
+from pydantic import BaseModel
 
 from commands.models import Type
 from database import session
@@ -10,7 +11,28 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-@cache(expire=200)
-async def get_databases():
-    return session.query(Type).first()
+@router.get("/type/{name}")
+@cache(expire=60)
+async def get_types(name: str):
+    result = session.query(Type).where(Type.type == name)
+    return {'type': result.all()}
+
+
+@router.get("/type")
+@cache(expire=60)
+async def get_all_types():
+    return {'type': session.query(Type).all()}
+
+
+class TypeBody(BaseModel):
+    name: str
+
+
+@router.post("/type")
+async def add_command(command_type: TypeBody):
+    name = command_type.name
+
+    session.add(Type(name=name))
+    session.commit()
+
+    return {'type': session.query(Type).where(name == name)}
