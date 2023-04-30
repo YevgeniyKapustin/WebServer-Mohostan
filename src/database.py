7f0feaf8c448c40d6d029fe58c098aaf93ea9d09
Yevgeniy Kap <kapustaevg@gmail.com>
2023-04-30 10:20:14 +0300

    add DB_URL to database

--- src/database.py
+++ src/database.py
@@ -1,13 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
-
-engine = create_engine(
-    f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-)
+from src.config import DB_URL
 
 Base = declarative_base()
-Base.metadata.create_all(engine)
 
+engine = create_engine(DB_URL)
 session = sessionmaker(bind=engine)()
