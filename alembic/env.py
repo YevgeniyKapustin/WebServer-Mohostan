da2f61bb84d9bf2c0ddfe584f782443c328f091c
Yevgeniy Kap <kapustaevg@gmail.com>
2023-04-30 11:39:20 +0300

    add readme to alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,17 +1,18 @@
-import os
 import sys
 from logging.config import fileConfig
+from pathlib import Path
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
 
-# sys.path.append(Path(sys.path[0]).joinpath('src'))
-sys.path.append(os.path.join(sys.path[0], 'src'))
+Path(sys.path[0]).joinpath('src')
 
 from src.config import DB_URL
 from src.database import Base
+
 from src.commands_types import models
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
