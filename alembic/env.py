c480ab2f111fdf09118e8e5b1fc10b039d35102e
Yevgeniy Kap <kapustaevg@gmail.com>
2023-04-30 11:22:12 +0300

    create type migrate

--- alembic/env.py
+++ alembic/env.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.join(sys.path[0], 'src'))
 
 from src.config import DB_URL
 from src.database import Base
-
+from src.commands_types import models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
--- src/commands_types/models.py
+++ src/commands_types/models.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, String, Integer, TEXT, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer
 
 from src.database import Base
 
@@ -9,23 +8,3 @@ class Type(Base):
 
     id = Column(Integer, primary_key=True, index=True, unique=True)
     name = Column(String(50), nullable=False, unique=True)
-
-
-class NormalRequest(Base):  # temporary
-    __tablename__ = 'normal_request'
-
-    id = Column(Integer, primary_key=True, index=True, unique=True)
-    type_id = Column(Integer, ForeignKey('Type.id'), nullable=False)
-    request = Column(TEXT, nullable=False)
-    response = Column(TEXT, nullable=False)
-    type = relationship('Type')
-
-
-class ContextualRequest(Base):  # temporary
-    __tablename__ = 'contextual_request'
-
-    id = Column(Integer, primary_key=True, index=True, unique=True)
-    type_id = Column(Integer, ForeignKey('Type.id'), nullable=False)
-    request = Column(TEXT, nullable=False)
-    response = Column(TEXT, nullable=False)
-    type = relationship('type')
