ba3b23a16174a9ba3371b39bf74a365bb3728f34
Yevgeniy Kap <kapustaevg@gmail.com>
2023-05-01 08:51:21 +0300

    add metadata

--- src/main.py
+++ src/main.py
@@ -7,7 +7,15 @@ from redis import asyncio as aioredis
 from src.commands_types.router import router as types_router
 
 app = FastAPI(
-    title='Ванесса'
+    title='Ванесса',
+    version='1.0',
+    description='API созданное для ванессы, впрочем, оно доступно всем',
+    openapi_tags=[
+        {
+            'name': 'Типы команд',
+            'description': 'Поле "type" для команд',
+        }
+    ],
 )
 app.include_router(types_router)
 
@@ -18,6 +26,7 @@ async def startup():
         'redis://localhost', encoding='utf8', decode_responses=True
     )
     FastAPICache.init(RedisBackend(redis), prefix='fastapi-cache')
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
