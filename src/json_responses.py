693e85dd666f3dc2028ebbfaa2e2bb64a2cee533
Yevgeniy Kap <kapustaevg@gmail.com>
2023-05-02 20:34:13 +0300

    add NoContentJSONResponse

--- src/json_responses.py
+++ src/json_responses.py
@@ -1,12 +1,14 @@
 """Стандартные HTTP json ответы."""
+from fastapi import HTTPException
 from starlette.responses import JSONResponse
 from starlette.status import (
-    HTTP_200_OK, HTTP_201_CREATED, HTTP_404_NOT_FOUND,
+    HTTP_200_OK, HTTP_201_CREATED, HTTP_404_NOT_FOUND, HTTP_204_NO_CONTENT,
     HTTP_422_UNPROCESSABLE_ENTITY
 )
 
 from src.schemas import (
-    CreateScheme, NotFoundScheme, UnpassableEntityScheme, OkScheme
+    CreateScheme, NotFoundScheme, UnpassableEntityScheme, OkScheme,
+    NoContentScheme
 )
 
 OkJSONResponse = JSONResponse(
@@ -17,6 +19,11 @@ OkJSONResponse = JSONResponse(
 CreateJSONResponse = JSONResponse(
     content=CreateScheme().dict(),
     status_code=HTTP_201_CREATED,
+)
+
+NoContentJSONResponse = JSONResponse(
+    content=NoContentScheme().dict(),
+    status_code=HTTP_204_NO_CONTENT,
 )
 
 NotFoundJSONResponse = JSONResponse(
