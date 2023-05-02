5017ee3570a1853fab929eed3636d9fecc261970
Yevgeniy Kap <kapustaevg@gmail.com>
2023-05-02 20:34:44 +0300

    add edit_type_name

--- src/commands_types/services.py
+++ src/commands_types/services.py
@@ -4,13 +4,19 @@ from src.commands_types.models import Type
 
 async def get_type_by_name(name: str) -> Type:
     return (
-        session.query(Type).where(
-            Type.name == name
-        ).first()
+        session.query(Type).
+        where(Type.name == name).
+        first()
     )
 
 
-async def create_type_by_name(name: str) -> bool:
+async def add_type_by_name(name: str) -> bool:
     session.add(Type(name=name))
+    session.commit()
+    return True
+
+
+async def edit_type_name(original_type: Type, new_name: str) -> bool:
+    original_type.name = new_name
     session.commit()
     return True
