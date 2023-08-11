a8ef2ea5fb11faa7f47ad6a51b2fab0e281a7858
Yevgeniy Kap <kapustaevg@gmail.com>
2023-08-11 12:22:12 +0300

    remove old imports

--- src/commands/models.py
+++ src/commands/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, TEXT, String
+from sqlalchemy import Column, TEXT, String
 
 from src.database import Base
 
--- src/videos/models.py
+++ src/videos/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 
 from src.database import Base
 
